2ac3b7c9e78c77eda06a16e1b6023acfb8fb2a72
dmsdaniel <dmsdaniel@gmail>
2018-01-20 22:22:45 -0200

    primeiro commit

---
+++ MDIForm/src/mdiform/AppCarregaParametros.java
@@ -0,0 +1,50 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mdiform;
+
+/**
+ *
+ * @author daniel
+ */
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
+public class AppCarregaParametros {
+  public static void main(String[] args) {
+
+	Properties prop = new Properties();
+	InputStream input = null;
+
+	try {
+
+		input = new FileInputStream("config.properties");
+
+		// load a properties file
+		prop.load(input);
+
+		// get the property value and print it out
+		System.out.println(prop.getProperty("database"));
+		System.out.println(prop.getProperty("dbuser"));
+		System.out.println(prop.getProperty("dbpassword"));
+
+	} catch (IOException ex) {
+		ex.printStackTrace();
+	} finally {
+		if (input != null) {
+			try {
+				input.close();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+
+  }
+   
+}
